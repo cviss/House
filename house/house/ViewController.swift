0e798d03f02bc8b01d81b0f755f2e9126cb0d6b8
Cole Weis <cviss@Lauras-MacBook-Air-2.local>
2016-07-17 22:27:05 +0200

    Initial Commit

---
+++ house/house/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  house
+//
+//  Created by Cole Weis on 7/17/16.
+//  Copyright © 2016 Carve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ house/houseTests/houseTests.swift
@@ -0,0 +1,36 @@
+//
+//  houseTests.swift
+//  houseTests
+//
+//  Created by Cole Weis on 7/17/16.
+//  Copyright © 2016 Carve. All rights reserved.
+//
+
+import XCTest
+@testable import house
+
+class houseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
