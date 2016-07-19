d2dfeef6bf6e01e02349e6a9f283de26b88e2946
Ira Quint <iraquint@Iras-MacBook-Pro.local>
2016-07-19 14:34:02 +0200

    added new storyboard

--- house/house/AppDelegate.swift
+++ house/house/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
         return true
     }
 
